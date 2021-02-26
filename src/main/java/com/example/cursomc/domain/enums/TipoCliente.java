840e97a8c1c1b144d99a3cc2d8daccb285c18873
Paulo Henrique Pereira <pharaujo14@gmail.com>
2021-02-26 18:04:24 -0300

    Cliente, TipoCliente, telefones e Endereco

---
+++ src/main/java/com/example/cursomc/domain/enums/TipoCliente.java
@@ -0,0 +1,42 @@
+package com.example.cursomc.domain.enums;
+
+public enum TipoCliente {
+	
+	PESSOAFISICA(1, "Pessoa Física"),
+	PESSOAJURIDICA(2, "Pessoa Jurídica");
+	
+	private Integer codigo;
+	private String descricao;
+	
+	private TipoCliente(Integer codigo, String descricao) {
+		this.codigo = codigo;
+		this.descricao = descricao;
+	}
+	
+	public Integer getCodigo() {
+		return codigo;
+	}
+	
+	public String getDescricao(){
+		return descricao;
+	}
+	
+	public static TipoCliente toEnum(Integer codigo) {
+		
+		if(codigo == null) {
+			return null;
+		}
+		
+		for(TipoCliente x : TipoCliente.values()) {
+			if(codigo.equals(x.getCodigo())) {
+				return x;
+			}
+		}
+		
+		throw new IllegalArgumentException("Id inválido: " + codigo);
+		
+	}
+	
+	
+
+}
